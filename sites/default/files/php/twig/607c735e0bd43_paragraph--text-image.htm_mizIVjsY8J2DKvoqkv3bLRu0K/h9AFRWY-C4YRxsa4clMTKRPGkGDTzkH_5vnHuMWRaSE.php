d8d72c272470a6a05afa71f54442ef7525c3290c
almantas <almantas06@gmail.com>
2021-04-18 21:03:03 +0300

    commit

---
+++ sites/default/files/php/twig/607c735e0bd43_paragraph--text-image.htm_mizIVjsY8J2DKvoqkv3bLRu0K/h9AFRWY-C4YRxsa4clMTKRPGkGDTzkH_5vnHuMWRaSE.php
@@ -0,0 +1,161 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/custom/tpk/templates/paragraph--text-image.html.twig */
+class __TwigTemplate_2ce2ff31795b499b1eecb7b7aa5a160ed8f1787175b1e223482b097ea8d3d9e9 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+            'paragraph' => [$this, 'block_paragraph'],
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 41
+        echo "
+";
+        // line 42
+        $this->displayBlock('paragraph', $context, $blocks);
+    }
+
+    public function block_paragraph($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 43
+        echo "    <div class=\"col-md-12\">
+      <div class=\"col-md-6 col-12 paragraph-type-text-image_image\">";
+        // line 44
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["content"] ?? null), "field_image", [], "any", false, false, true, 44), 44, $this->source), "html", null, true);
+        echo "</div>
+      <div class=\"col-md-6 col-12\">
+        <h2>";
+        // line 46
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["content"] ?? null), "field_title", [], "any", false, false, true, 46), 46, $this->source), "html", null, true);
+        echo "</h2>
+        ";
+        // line 47
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["content"] ?? null), "field_text_long", [], "any", false, false, true, 47), 47, $this->source), "html", null, true);
+        echo "
+      </div>
+    </div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/custom/tpk/templates/paragraph--text-image.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  62 => 47,  58 => 46,  53 => 44,  50 => 43,  43 => 42,  40 => 41,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{#
+/**
+ * @file
+ * Default theme implementation to display a paragraph.
+ *
+ * Available variables:
+ * - paragraph: Full paragraph entity.
+ *   Only method names starting with \"get\", \"has\", or \"is\" and a few common
+ *   methods such as \"id\", \"label\", and \"bundle\" are available. For example:
+ *   - paragraph.getCreatedTime() will return the paragraph creation timestamp.
+ *   - paragraph.id(): The paragraph ID.
+ *   - paragraph.bundle(): The type of the paragraph, for example, \"image\" or \"text\".
+ *   - paragraph.getOwnerId(): The user ID of the paragraph author.
+ *   See Drupal\\paragraphs\\Entity\\Paragraph for a full list of public properties
+ *   and methods for the paragraph object.
+ * - content: All paragraph items. Use {{ content }} to print them all,
+ *   or print a subset such as {{ content.field_example }}. Use
+ *   {{ content|without('field_example') }} to temporarily suppress the printing
+ *   of a given child element.
+ * - attributes: HTML attributes for the containing element.
+ *   The attributes.class element may contain one or more of the following
+ *   classes:
+ *   - paragraphs: The current template type (also known as a \"theming hook\").
+ *   - paragraphs--type-[type]: The current paragraphs type. For example, if the paragraph is an
+ *     \"Image\" it would result in \"paragraphs--type--image\". Note that the machine
+ *     name will often be in a short form of the human readable label.
+ *   - paragraphs--view-mode--[view_mode]: The View Mode of the paragraph; for example, a
+ *     preview would result in: \"paragraphs--view-mode--preview\", and
+ *     default: \"paragraphs--view-mode--default\".
+ * - view_mode: View mode; for example, \"preview\" or \"full\".
+ * - logged_in: Flag for authenticated user status. Will be true when the
+ *   current user is a logged-in member.
+ * - is_admin: Flag for admin user status. Will be true when the current user
+ *   is an administrator.
+ *
+ * @see template_preprocess_paragraph()
+ *
+ * @ingroup themeable
+ */
+#}
+
+{% block paragraph %}
+    <div class=\"col-md-12\">
+      <div class=\"col-md-6 col-12 paragraph-type-text-image_image\">{{ content.field_image }}</div>
+      <div class=\"col-md-6 col-12\">
+        <h2>{{ content.field_title }}</h2>
+        {{ content.field_text_long }}
+      </div>
+    </div>
+{% endblock paragraph %}
+", "themes/custom/tpk/templates/paragraph--text-image.html.twig", "/Applications/MAMP/htdocs/drupal/themes/custom/tpk/templates/paragraph--text-image.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("block" => 42);
+        static $filters = array("escape" => 44);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['block'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
